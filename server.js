c4e46640163cc755c9bae1b29847fbc1e0d7b732
Volodymyr Nerovnia <nerv@i.ua>
2024-06-29 18:26:41 +0300

    Created simple express server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 'use strict'
 
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json(), cors());
+
+app.listen(port, () => console.log(`Server run at port ${port}`));
+
