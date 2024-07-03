3e5ad1c06d5c0967d3cbb73e1e4abed1d9c00897
Volodymyr Nerovnia <nerv@i.ua>
2024-07-03 23:47:21 +0300

    Created service.js

---
+++ service.js
@@ -0,0 +1,33 @@
+import { getObjFromJSONFile } from './utils/json-data.js';
+
+
+const jsonFileName = './data/index.json';
+
+const jsonData = getObjFromJSONFile(jsonFileName);
+
+const jsonObj = jsonData?.contents;
+
+if (!jsonObj) exit; 
+const titles = jsonObj.map((d) => d.title);
+
+
+const getContent = (obj, key) => {
+  return obj[key];
+}
+
+const getTitles = () => titles;
+const getChapter = (chapter) => {
+  const arr = jsonObj.filter(o => o.title === chapter);
+  if (arr.length > 0) {
+    return arr[0];
+  }
+  return null;
+}
+const getParagraph = (chapter, paragraph) => {
+  const ch = getChapter(chapter);
+  if (!ch) return null;
+  return ch[paragraph];
+};
+
+//console.log(getChapter('Good luck!'))
+//console.log(getParagraph('Placing an order', 'Cancel an order'));
